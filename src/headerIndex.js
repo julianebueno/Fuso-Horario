5f8ce813c7f1d275a462df315118f06691ac9403
Juliane Bueno <julianebueno1@gmail.com>
2025-01-13 04:45:07 -0300

    update header geral

---
+++ src/headerIndex.js
@@ -0,0 +1,10 @@
+const headerIndex = document.getElementById('headerIndex');
+
+headerIndex.innerHTML = `
+  <h1>DAHORA</h1>
+  <nav>
+    <a href="./">Inicial</a>
+    <a href="./pages/horaCidade.html">Hora por Cidade</a>
+    <a href="./pages/horaUtc.html">Hora por UTC</a>
+  </nav>
+`;
