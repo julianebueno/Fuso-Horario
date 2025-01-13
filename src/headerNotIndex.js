42ec12bf5765cdc14502594d67678317fab6623c
Juliane Bueno <julianebueno1@gmail.com>
2025-01-13 04:59:31 -0300

    fix link

--- src/headerNotIndex.js
+++ src/headerNotIndex.js
@@ -3,7 +3,7 @@ const headerNotIndex = document.getElementById('headerNotIndex');
 headerNotIndex.innerHTML = `
   <h1>DAHORA</h1>
   <nav>
-    <a href="../../">Inicial</a>
+    <a href="../">Inicial</a>
     <a href="./horaCidade.html">Hora por Cidade</a>
     <a href="./horaUtc.html">Hora por UTC</a>
   </nav>
